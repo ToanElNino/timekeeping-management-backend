fbbe8f6f115f215b930955e6016957cf8215144d
toanelnino <toannguyen@var-meta.com>
2023-06-29 22:32:12 +0700

    calculate day working time sheet

--- src/modules/timesheet-worker/timesheet-worker.service.ts
+++ src/modules/timesheet-worker/timesheet-worker.service.ts
@@ -4,9 +4,6 @@ import {InjectQueue} from '@nestjs/bull';
 import {InjectRepository} from '@nestjs/typeorm';
 import {CheckinLog, Schedule, TimeSheet} from 'src/database/entities';
 import {Repository} from 'typeorm';
-import {nowInMillis} from 'src/shared/Utils';
-import {now} from 'lodash';
-import {time} from 'console';
 
 @Injectable()
 export class TimeSheetWorkerService {
@@ -74,111 +71,20 @@ export class TimeSheetWorkerService {
       timeSheet,
       schedule
     );
-    // else if (checkinLogListUser.length === 1) {
-    //   console.log('1');
-    // } else {
-    //   console.log('2');
-    //   const listCheckinTime = [];
-    //   checkinLogListUser.forEach((e: CheckinLog) =>
-    //     listCheckinTime.push(Number(e.timeRecordNumber))
-    //   );
-    //   const {checkinTime, checkoutTime} =
-    //     this.getCheckinAndCheckoutTimeFromListTime(listCheckinTime);
-
-    //   // const tmp = `${day.substring(6, 10)}-${day.substring(
-    //   //   3,
-    //   //   5
-    //   // )}-${day.substring(0, 2)}`;
-    //   // //không cần cập nhật
-    //   // if (
-    //   //   timeSheet.timeCome === checkinTime &&
-    //   //   timeSheet.timeLeave === checkoutTime
-    //   // ) {
-    //   //   return;
-    //   // }
-    //   // await this.handleTimeSheetFromCheckinOutTime(
-    //   //   timeSheet,
-    //   //   checkinTime,
-    //   //   checkoutTime,
-    //   //   schedule,
-    //   //   tmp
-    //   // );
-    //   // console.log(listCheckinTime);
-    // }
+    console.log('morningTimesheet: ', morningTimesheet);
+    if (!stop) {
+      const calculatedTimesheet = await this.calculateTimeSheetAfternoon(
+        checkinTime,
+        checkoutTime,
+        morningTimesheet,
+        schedule
+      );
+      console.log('calculatedTimesheet: ', calculatedTimesheet);
+    }
   }
   millisSecondToMinutes = (millis: number) => {
     return millis / (1000 * 60);
   };
-  // async handleTimeSheetFromCheckinOutTime(
-  //   timeSheet: TimeSheet,
-  //   checkinTime: number,
-  //   checkoutTime: number,
-  //   schedule: Schedule,
-  //   today: string
-  // ) {
-  //   console.log(today);
-  //   const currentTime = new Date().getTime();
-  //   console.log(currentTime);
-  //   const timeStart1 = new Date(today + ' ' + schedule.timeStart1).getTime();
-  //   const timeEnd1 = new Date(today + ' ' + schedule.timeEnd1).getTime();
-  //   const timeComeOff1 = new Date(
-  //     today + ' ' + schedule.timeComeOff1
-  //   ).getTime();
-  //   const timeStart2 = new Date(today + ' ' + schedule.timeStart1).getTime();
-  //   const timeEnd2 = new Date(today + ' ' + schedule.timeEnd1).getTime();
-  //   // const timeDelay;
-
-  //   //tính checkin time và checkout time trước
-  //   // đang trong thời gian làm việc => chỉ cập nhật checkin time
-  //   if (currentTime < timeEnd2) {
-  //     if (timeSheet.timeCome !== checkinTime)
-  //       await this.timesheetRepo.save({
-  //         ...this.timesheetQueue,
-  //         timeCome: checkinTime,
-  //       });
-  //     return;
-  //   }
-  //   //hết thời gian làm việc => cập nhật cả 2
-  //   if (currentTime > timeEnd2) {
-  //     console.log('123');
-  //     console.log(typeof timeSheet.timeCome);
-  //     console.log(typeof checkinTime);
-  //     if (Number(timeSheet.timeCome) !== checkinTime) {
-  //       console.log('114');
-  //       timeSheet.timeCome = checkinTime;
-  //       //update thời gian vào
-  //       //có làm việc buổi sáng
-  //       if (checkinTime <= timeEnd1) {
-  //         console.log('115');
-  //         //đúng giờ
-  //         if (checkinTime <= timeStart1) {
-  //           console.log('116');
-  //           timeSheet.comeLateMinutes = 0;
-  //           timeSheet.workingDay1 = 0.5;
-  //         }
-  //         //muộn giờ vẫn có lương
-  //         if (checkinTime > timeStart1 && checkinTime < timeComeOff1) {
-  //           console.log('117');
-  //           timeSheet.workingDay1 = 0.25;
-  //           timeSheet.comeLateMinutes = this.millisSecondToMinutes(
-  //             checkinTime - timeStart1
-  //           );
-  //         }
-  //         //không được tính lương
-  //         if (checkinTime > timeComeOff1 && checkinTime < timeEnd1) {
-  //           timeSheet.comeLateMinutes = this.millisSecondToMinutes(
-  //             checkinTime - timeStart1
-  //           );
-  //           timeSheet.workingDay1 = 0;
-  //         }
-  //       }
-  //       await this.timesheetRepo.save(timeSheet);
-  //     }
-  //     if (timeSheet.timeCome !== checkinTime) {
-  //       timeSheet.timeCome = checkinTime;
-  //     }
-  //   }
-  // }
 
   getCheckinAndCheckoutTimeFromListTime(listTime: number[]) {
     let min = listTime[0];
@@ -253,11 +159,11 @@ export class TimeSheetWorkerService {
     // eslint-disable-next-line no-empty
     //về sớm không được tính lương buổi sáng
     if (checkoutTime <= timeStampLeaveOff) {
-      maxWorkingDay = -0.5;
+      maxWorkingDay -= 0.5;
     }
     //về sớm bị trừ nửa lương sáng
     if (checkoutTime > timeStampLeaveOff && checkoutTime < timeStampEarly) {
-      maxWorkingDay = -0.25;
+      maxWorkingDay -= 0.25;
     }
     //đến muộn, không được tính công buổi sáng
     else if (checkinTime > timeStampComeOff) {
@@ -272,6 +178,92 @@ export class TimeSheetWorkerService {
       ...timesheet,
     });
     return {morningTimesheet: timesheet, stop: false};
+  }
+
+  //tính công cho buổi chiều => chắc chắn k trong thời gian đang làm buổi sáng
+  async calculateTimeSheetAfternoon(
+    checkinTime: number,
+    checkoutTime: number,
+    timesheet: TimeSheet,
+    schedule: Schedule
+  ) {
+    const timeStampNow = new Date().getTime();
+    const timeStampEnd = this.convertScheduleStringToTimeStamp(
+      schedule.timeEnd2
+    );
+    // đang trong giờ làm buổi chiều, chắc chắn timeStampNow -> time end của buỏi sáng
+    //checkin time đã được cập nhật từ morning nên k cần update checkin time nữa
+    // if (timeStampNow < timeStampEnd) {
+    //   // update thời gian checkin
+    //   if (checkinTime !== timesheet.timeCome)
+    //     timesheet.timeCome = Number(checkinTime);
+    //   await this.timesheetRepo.save({
+    //     ...timesheet,
+    //   });
+    //   return {morningTimesheet: timesheet, stop: true};
+    // }
+    //qua thời gian làm buổi sáng => tính time sheet cho buổi sáng
+    ////////check thời gian checkin
+    let maxWorkingDay = 0.5;
+    const timeStampStart = this.convertScheduleStringToTimeStamp(
+      schedule.timeStart2
+    );
+    const timeStampDelay = this.convertScheduleStringToTimeStamp(
+      schedule.timeDelay2
+    );
+
+    const timeStampComeOff = this.convertScheduleStringToTimeStamp(
+      schedule.timeComeOff2
+    );
+    // eslint-disable-next-line no-empty
+    if (checkinTime <= timeStampDelay) {
+      maxWorkingDay -= 0;
+      console.log('come in time');
+    }
+    //đến muộn, bị trừ nửa công buổi chiều
+    else if (checkinTime > timeStampDelay && checkinTime <= timeStampComeOff) {
+      maxWorkingDay -= 0.25;
+    }
+    //đến muộn, không được tính công buổi chiều
+    else if (checkinTime > timeStampComeOff) {
+      maxWorkingDay -= 0.5;
+    }
+    /////////check thời gian check out
+    const timeStampLeaveOff = this.convertScheduleStringToTimeStamp(
+      schedule.timeLeaveOff2
+    );
+    const timeStampEarly = this.convertScheduleStringToTimeStamp(
+      schedule.timeEarly2
+    );
+
+    // const timeStampComeOff = this.convertScheduleStringToTimeStamp(
+    //   schedule.timeComeOff1
+    // );
+    // eslint-disable-next-line no-empty
+    //về sớm không được tính lương buổi chiều
+    if (checkoutTime <= timeStampLeaveOff) {
+      maxWorkingDay -= 0.5;
+    }
+    //về sớm bị trừ nửa lương chiều
+    else if (
+      checkoutTime > timeStampLeaveOff &&
+      checkoutTime < timeStampEarly
+    ) {
+      maxWorkingDay -= 0.25;
+    }
+    //đến muộn, không được tính công buổi sáng
+    else if (checkoutTime >= timeStampEarly) {
+      maxWorkingDay -= 0;
+    }
+
+    //tính công buổi chiều
+    maxWorkingDay = maxWorkingDay ?? 0;
+    timesheet.workingDay2 = maxWorkingDay;
+    timesheet.timeLeave = checkoutTime;
+    await this.timesheetRepo.save({
+      ...timesheet,
+    });
+    return timesheet;
   }
 
   async getCheckinLogForUser(tenantId: number, userId: number, day: string) {
